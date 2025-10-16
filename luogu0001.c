44ba4a6471a67ceef78e692a84e5a5bef337e2da
ElonFreeman <zhanghao112358@outlook.com>
2025-10-16 18:09:36 +0800

    Add files via upload

---
+++ luogu0001.c
@@ -0,0 +1,24 @@
+// Created by freeman on 2025/10/16.
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+
+  scanf("%d%d",&a,&b);
+
+/*
+ *printf("InputA:\n");
+  scanf("%d",&a);
+
+printf("InputB:\n");
+  scanf("%d",&b);
+
+  printf("A+B=%d",a+b);
+*/
+  printf("%d",a+b);
+
+
+  return 0;
+
+}
---
+++ luogu0002.c
@@ -0,0 +1,14 @@
+// Created by freeman on 2025/10/16.
+#include <stdio.h>
+
+int main()
+{
+    int n,k;
+    scanf("%d%d",&n,&k);
+
+    int total=n+n/k;
+    
+
+    printf("%d",total);
+    return 0;
+}
